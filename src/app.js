1bceaf4df40517daf3b303d20222ad7f9fbc870e
AChareun <achareun22@gmail.com>
2020-09-01 16:06:24 -0300

    set up boilerplate express app with nunjucks as template engine

---
+++ src/app.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const express = require('express');
+const nunjucks = require('nunjucks');
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use('/public', express.static('public'));
+
+nunjucks.configure('src/module', {
+  autoescape: true,
+  express: app
+});
+
+app.get('/', (req, res) => {
+  res.render('view/layout/index.html')
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
+})
