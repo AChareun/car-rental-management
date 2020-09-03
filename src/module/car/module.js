1048a0dc942c4173a99515a848c3e98c665424b2
AChareun <achareun22@gmail.com>
2020-09-03 15:08:03 -0300

    implement car module init

---
+++ src/module/car/module.js
@@ -0,0 +1,27 @@
+/**
+ * @typedef {import('express').Application} ExpressApp
+ * @typedef {import('rsdi').IDIContainer} IDIContainer
+ */
+
+const CarController = require('./controller/carController');
+const CarService = require('./service/carService');
+const CarRepository = require('./repository/sqlite/carRepository');
+
+/**
+ * @param {ExpressApp} app
+ * @param {IDIContainer} container
+ */
+function carModuleInit(app, container) {
+  /**
+   * @type {CarController} carController
+   */
+  const carController = container.get('CarController');
+  carController.configureRoutes(app);
+}
+
+module.exports = {
+  carModuleInit,
+  CarController,
+  CarService,
+  CarRepository,
+};
