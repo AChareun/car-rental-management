0e4fb5a3029bf58d12070f737ddb3ba98c5d9263
AChareun <achareun22@gmail.com>
2020-09-15 11:42:35 -0300

    add test for CarModel

---
+++ src/module/car/model/__test__/carModel.test.js
@@ -0,0 +1,13 @@
+const { Sequelize } = require('sequelize');
+
+const CarModel = require('../carModel');
+
+const sequelizeInstance = new Sequelize('sqlite::memory:');
+
+test('CarModel setup creates Cars table on database', async () => {
+  CarModel.setup(sequelizeInstance);
+
+  await sequelizeInstance.sync({ force: true });
+
+  expect(CarModel.findAll()).resolves.toEqual([]);
+});
